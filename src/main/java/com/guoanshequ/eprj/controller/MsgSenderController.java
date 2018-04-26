1fad23754ce4e28a8f35024028ac914061779f59
zhaoxuguang <zhaoxuguang@guoanshequ.com>
2018-04-26 17:05:59 +0800

    添加统计发送短信记录功能

--- src/main/java/com/guoanshequ/eprj/controller/MsgSenderController.java
+++ src/main/java/com/guoanshequ/eprj/controller/MsgSenderController.java
@@ -1,11 +1,17 @@
 package com.guoanshequ.eprj.controller;
 
+import java.io.File;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.guoanshequ.eprj.config.EprjConfig;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -28,10 +34,20 @@ public class MsgSenderController {
 	private EprjConfig eprjConfig;
 
 	static Logger logger = LoggerFactory.getLogger(MsgSenderController.class);
-
+	
+	//static String url = System.getProperty("user.dir");
+	//static File file = new File(url+File.separator+".."+File.separator+"sms_counter.log");
+	static File file = null;
+	
 	@RequestMapping("/dispatcher.action")
 	public String sendRequest(@RequestParam("phone") String phoneNumber,
 			@RequestParam("sendcode") String messageContent) {
+		
+		if (file == null) {
+		    file = new File(request.getServletContext().getRealPath("/") + File.separator+".." + File.separator+"sms-counter.log");
+		}
+		
+		System.out.println("-------------"+request.getServletContext().getRealPath("/") + File.separator+".." + File.separator+"sms-counter.log");
 		
 		logger.info(String.format("\nmessage detail:\nphone:\t%s\nmessage:\t%s\n", phoneNumber, messageContent));
 
@@ -41,13 +57,14 @@ public class MsgSenderController {
 			try {
 				final String requestUrlTpl = "http://q.hl95.com:8061/?username=gasjyz&password=Gasj0121&message=%s&phone=%s&epid=123743&linkid=&subcode=";
 				String messageContent_gb2312 = URLEncoder.encode(messageContent, "gb2312");
-				
 				CloseableHttpClient httpclient = HttpClients.createDefault();
 				HttpGet httpGet = new HttpGet(String.format(requestUrlTpl, messageContent_gb2312, phoneNumber));
 				CloseableHttpResponse response = httpclient.execute(httpGet);
 				resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+				String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date())+"\t"+phoneNumber+"\t"+resultString+"\t"+messageContent+System.getProperty("line.separator");
 				logger.info(resultString);
 				response.close();
+				FileUtils.writeStringToFile(file, date, Charset.defaultCharset(), true);
 			} catch (Exception e) {
 				e.printStackTrace();
 				resultString = "Error:" + e.getMessage();
