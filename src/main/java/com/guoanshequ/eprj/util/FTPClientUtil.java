c5e547e01c19f3471188c8b834257fea126fc5fa
gaobaolei <2636130684@qq.com>
2018-11-14 18:19:56 +0800

    ftpstore

--- src/main/java/com/guoanshequ/eprj/util/FTPClientUtil.java
+++ src/main/java/com/guoanshequ/eprj/util/FTPClientUtil.java
@@ -102,6 +102,7 @@ public class FTPClientUtil {
         try {
             fileInputStream =new  FileInputStream(localFile);
             ftpClient.changeWorkingDirectory(remoteFileDir);
+            ftpClient.enterLocalPassiveMode();
             uploadResult = ftpClient.storeFile(remoteFile, fileInputStream);
             System.out.printf("upload result: %s.\n", uploadResult);
             fileInputStream.close();
