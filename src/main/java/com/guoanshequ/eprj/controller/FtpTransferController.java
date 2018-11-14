6adf20a0e8ffc76a285bbbe8db9f0d0fa7c585ce
gaobaolei <2636130684@qq.com>
2018-11-14 14:01:02 +0800

    jgjc csv 文件上传至发改委ftp

--- src/main/java/com/guoanshequ/eprj/controller/FtpTransferController.java
+++ src/main/java/com/guoanshequ/eprj/controller/FtpTransferController.java
@@ -1,5 +1,7 @@
 package com.guoanshequ.eprj.controller;
 
+import com.guoanshequ.eprj.util.FTPClientUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,13 +23,26 @@ import java.util.Map;
 @RestController
 public class FtpTransferController {
 
+    @Value("${eprj.ndrc.ftpServer}")
+    private String ftpServer;
+
+    @Value("${eprj.ndrc.userName}")
+    private String ftpUserName;
+
+    @Value("${eprj.ndrc.passWord}")
+    private String ftpPassWord;
+
+    @Value("${eprj.ndrc.fileDir}")
+    private String remoteFileDir;
+
     @RequestMapping(value = "/upload", consumes = "multipart/form-data", method = RequestMethod.POST)
     public void transfer2Ftp(@RequestParam("file")MultipartFile file) {
 
         System.out.printf("%s\n", file.getOriginalFilename());
         // 判断文件是否存在
         if (!file.isEmpty()) {
-            String path = "d:/upload/";
+
+            String path = "/jgjc_upload/";
             File dir = new File(path);
             if (!dir.exists()) {
                 dir.mkdirs();
@@ -37,6 +52,9 @@ public class FtpTransferController {
             File localFile = new File(path);
             try {
                 file.transferTo(localFile);
+                FTPClientUtil ftpClientUtil = new FTPClientUtil(ftpServer,ftpUserName,ftpPassWord,remoteFileDir);
+                ftpClientUtil.uploadFile(localFile,localFile.getName());
+                ftpClientUtil.closeConnect();
             } catch (IllegalStateException | IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -46,7 +64,7 @@ public class FtpTransferController {
 
     @RequestMapping(value = "/upload-multi", consumes = "multipart/form-data", method = RequestMethod.POST)
     public void transfer2Ftp(HttpServletRequest request) {
-        String pathname = "d:/upload/";
+        String pathname = "/jgjc_upload/";
         File file = new File(pathname);
         if (!file.exists()) {
             file.mkdirs();
@@ -54,14 +72,19 @@ public class FtpTransferController {
         MultipartHttpServletRequest muti = (MultipartHttpServletRequest) request;
         System.out.println(muti.getMultiFileMap().size());
 
+        FTPClientUtil ftpClientUtil = new FTPClientUtil(ftpServer,ftpUserName,ftpPassWord,remoteFileDir);
+
         MultiValueMap<String, MultipartFile> map = muti.getMultiFileMap();
         for (Map.Entry<String, List<MultipartFile>> entry : map.entrySet()) {
 
             List<MultipartFile> list = entry.getValue();
             for (MultipartFile multipartFile : list) {
                 try {
-                    multipartFile.transferTo(new File(pathname
-                            + multipartFile.getOriginalFilename()));
+
+                    File uploadFile = new File(pathname + multipartFile.getOriginalFilename());
+                    multipartFile.transferTo(uploadFile);
+                    ftpClientUtil.uploadFile(uploadFile,uploadFile.getName());
+                    ftpClientUtil.closeConnect();
                 } catch (IllegalStateException | IOException e) {
                     e.printStackTrace();
                 }
